7484604060284470fac39679e250278cd680c56d
twice <dvajdi@bk.ru>
2016-03-15 15:41:39 +0300

    first

--- app/src/main/java/com/example/twice/fornewwork/html_activity.java
+++ app/src/main/java/com/example/twice/fornewwork/html_activity.java
@@ -1,12 +1,16 @@
 package com.example.twice.fornewwork;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class html_activity extends AppCompatActivity {
     WebView webView;
@@ -15,13 +19,30 @@ public class html_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.html_activity);
-        WebView webView = (WebView)findViewById(R.id.webView);
+
+        webView = (WebView)findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new MyWebView());
 
         String str = getIntent().getStringExtra("URL");
 
-
-
         webView.loadUrl(str);
+
+
+        Log.d("priv",""+webView.toString());
+
     }
 
+    class MyWebView extends WebViewClient{
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+    }
 }
